7ad1584423fe52067b74cddf9edd936d5b58dd03
Hayato.Tanoue <hayato.tanoue@g.softbank.co.jp>
2025-05-17 11:15:03 +0900

    first commit

---
+++ tanoue_codes/run.sh
@@ -0,0 +1,12 @@
+# 割り当てGPUを選択できるようにする(複数)
+USE_GPU_IDS=${1:-0}
+
+# docker run コマンドを実行するスクリプト
+docker run --gpus '"device='"$USE_GPU_IDS"'"' \
+    --rm \
+    -it \
+    -e NVIDIA_DRIVER_CAPABILITIES=all \
+    -v ${NAS_PATH}:${NAS_PATH} \
+    -v ${PWD}:/workspace \
+    --shm-size=200G \
+    ipercore:success bash
